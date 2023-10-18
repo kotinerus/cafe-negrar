eaa85f4ab9a91f9086b2000066d13952f347cb12
Krzysztof Wysocki <kotinerus@gmail.com>
2023-10-18 10:45:19 +0200

    Added panel component + changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import coffeBg from "./coffee-beans-background.png";
 import waiter from "./waiterJPG.jpeg";
 import send from "./send.png";
+import workingMen from "./working-men.jpg";
 import "./scss/AppStyles.css";
 export default function App() {
   return (
     <>
       <Navbar />
+      {/* POCZĄTEK DO POPRAWY */}
       <div className="welcomeDiv"></div>
 
       <div className="welcomeDiv_text">
@@ -18,16 +20,50 @@ export default function App() {
           <button>WYŚLIJ</button>
         </form>
       </div>
+      {/* KONIEC DO POPRAWY */}
 
-      <div className="menuDiv">
-        <div className="menuDiv_text">
-          <h2>Zasmakuj się w naszej kawie</h2>
+      <PanelImgRight>
+        <section>
+          <h2>Rozsmakuj się w naszej kawie</h2>
           <p>
-            Nie ma nic lepszego niż przepyszna kawa ze świeżo palonych ziaren
+            Nasza kawiarnia to miejsce, gdzie aromatyczne ziarna spotykają się z
+            pasją i precyzją, tworząc niezapomniane smaki.
           </p>
-        </div>
+          <p>
+            Odkryj różnorodność naszego menu, które zaspokoi każdą kawową
+            zachciankę i pozwoli Ci na prawdziwą podróż przez świat kawowych
+            doznań.
+          </p>
+          <Button>Przejdź do menu</Button>
+        </section>
         <img src={coffeBg} />
-      </div>
+      </PanelImgRight>
+
+      <PanelImgLeft panelPallete="panel-secondary">
+        <img src={workingMen} />
+        <section>
+          <h2>
+            Kreuj historię w <span>naszej</span> przestrzeni
+          </h2>
+          <p>
+            Pracuj w otoczeniu pełnym innowacji, kreatywności i możliwości.
+            Nasza przestrzeń to więcej niż biuro - to centrum, w którym Twoje
+            pomysły nabierają życia.
+          </p>
+          <p>
+            Tu spotkasz inspirujących ludzi, zbudujesz swoją markę i osiągniesz
+            sukces. Rezerwuj swój stolik już teraz i stań się częścią historii
+            sukcesu, która tworzy się właśnie w naszej przestrzeni.
+          </p>
+          <p>Twój sukces zaczyna się dziś.</p>
+          <Button>Rezerwuj teraz</Button>
+        </section>
+      </PanelImgLeft>
+
+      <PanelImgRight>
+        <section>TEST</section>
+        <img src={workingMen} />
+      </PanelImgRight>
     </>
   );
 }
@@ -40,4 +76,28 @@ function Navbar() {
       <h2>Oferta</h2>
     </nav>
   );
+}
+
+function Panel({ backgroundColor = "#2b2828", color = "#f4eded", children }) {
+  return (
+    <div
+      style={{
+        backgroundColor: backgroundColor,
+        color: color,
+      }}
+      className="panel  "
+    >
+      {children}
+    </div>
+  );
+}
+
+function PanelImgLeft({ panelPallete = "panel-primary", children }) {
+  return <div className={`panel ${panelPallete} imgLeft`}>{children}</div>;
+}
+function PanelImgRight({ panelPallete = "panel-primary", children }) {
+  return <div className={`panel ${panelPallete} imgRight`}>{children}</div>;
+}
+function Button({ children }) {
+  return <button className="btn-info">{children}</button>;
 }
